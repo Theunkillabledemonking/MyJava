9557d932755d7ed37760bd3c88f8c5980b123c28
Theunkillabledemonking <abcqkdnxm@g.yju.com>
2024-10-22 10:02:12 +0900

    1018

--- src/cobo/Chap4/Ex4_11.java
+++ src/cobo/Chap4/Ex4_11.java
@@ -4,7 +4,7 @@ public class Ex4_11 {
   public static void main(String[] args) {
     for(int i=1; i<=5; i++){
       System.out.print("*");
-      for(int j=1; j<=10; j++){
+      for(int j=1; j<=i; j++){
         System.out.print("*");
       }
       System.out.println();
