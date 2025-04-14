11769b02f1389e264d68368b6d5d25ef1f3c5ed7
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-04-14 15:51:24 +0900

    oop의 상속의 오버라이딩 및 접근제어자 내용 학습.

---
+++ src/OOP/prt240414.java
@@ -0,0 +1,28 @@
+package src.OOP;
+
+// 반환형, 이름, 매개변수가 부모에서 정의된 메소드와 일치
+// 예외, 접근제어자
+
+class Bar1 {
+    private int score;
+
+    // Getter, Setter Method
+    int getScore() { return score; }
+    boolean setScore(int newScore) {
+        if (newScore >= 0 && newScore <= 100) {
+            score = newScore;
+            return true;
+        }
+        return false;
+    }
+}
+
+
+
+public class prt240414 {
+    public static void main(String[] args) {
+        Bar1 b = new Bar1();
+        b.setScore(100);
+        b.setScore(-100);
+    }
+}
