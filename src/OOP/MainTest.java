2e9461b721bac1757f7a23aa5032edda213488f5
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-05-07 15:54:03 +0900

    인터페이스의 접근제어자

--- src/OOP/MainTest.java
+++ src/OOP/MainTest.java
@@ -1,58 +1,48 @@
 package src.OOP;
 
-class A { int value = 1;}
-class B extends A {
-    int value;
-    B(int value) {
-        this.value = value;
+
+interface Scan {
+    // public abstract void doScan()
+    void doScan();
+    // public static final int scanNum = 2;
+    int scanNum = 2;
+
+    default void prePaper() {
+        powerOn();
+        System.out.println("Pre-paper");
     }
-}
-class C extends A { int value = 3;}
-class D extends B {
-    int value = 4;
-    D() {
-        super(3);
+
+    static void prePower() {
+        System.out.println("Pre-power");
     }
-    void prtValue() {
-        System.out.println(super.value);
-        System.out.println(value);
+
+    private void powerOn() {
+        System.out.println("Power on");
     }
 }
 
+class Equipment implements Scan {
+    public void doScan() {
+        System.out.println("Equipment scan");
+        Scan.prePower();
+    }
+}
+
+class Device implements Scan {
+    int scanNum = 10;
+    @Override
+    public void doScan() {
+        System.out.println(Scan.scanNum);
+    }
+
+    @Override
+    public void prePaper() {
+        Scan.super.prePaper(); // 부분 재정립 가능
+
+    }
+}
 public class MainTest {
     public static void main(String[] args) {
-        C c = new C();
-        D d = new D();
 
-        d.prtValue();
-    }
-}
-package src.OOP;
-
-class A { int value = 1;}
-class B extends A {
-    int value;
-    B(int value) {
-        this.value = value;
-    }
-}
-class C extends A { int value = 3;}
-class D extends B {
-    int value = 4;
-    D() {
-        super(3);
-    }
-    void prtValue() {
-        System.out.println(super.value);
-        System.out.println(value);
-    }
-}
-
-public class MainTest {
-    public static void main(String[] args) {
-        C c = new C();
-        D d = new D();
-
-        d.prtValue();
     }
 }
